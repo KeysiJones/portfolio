721338b14f14819f2a75f83a85b6efb9a65dd07d
keysi.fernandes <keysi.fernandes@hypeflame.com.br>
2021-07-23 19:50:34 -0300

    subindo portfolio

---
+++ src/components/Box.tsx
@@ -0,0 +1,30 @@
+
+function Box (
+    props: {
+        text: string,
+        bg: string,
+        hoverBg: string,
+        style: string,
+        link?: string
+    }) {
+    
+    const classe = `transition duration-150 transform hover:scale-110 ${props.bg} hover:${props.hoverBg} rounded-lg ${props.style} mx-auto`
+    
+    const openLink = (link: string) => {
+        if(props.link){
+            if(props.link.startsWith('#')) {
+                window.location.href = link
+                return
+            }
+            window.open(link, '_blank')
+        }
+    }
+
+    return (
+        <button className={classe} onClick={() => openLink(props.link ?? '')}>
+            <p className='text-3xl text-white'>{props.text}</p>
+        </button>
+    )
+}
+
+export default Box
